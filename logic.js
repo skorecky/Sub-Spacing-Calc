1dba022cae1d8c33540811351edde6b4a80163be
Stephen Korecky <skorecky@gmail.com>
2022-08-27 01:26:59 -0500

    roudning to nearist decimal and making inputs number

--- logic.js
+++ logic.js
@@ -5,24 +5,24 @@ var total_sub_width = function(sub_width, sub_stacks){
 };
 
 var sub_spaces = function(sub_stacks){
-  return (sub_stacks - 1)
+  return (sub_stacks - 1).toFixed(2)
 };
 
 var get_starting_point = function(sub_width){
-  return (sub_width / 2)
+  return (sub_width / 2).toFixed(2)
 };
 
 var calculate_spacing = function(stage_width, sub_width, sub_stacks){
   var sub_spacing = sub_spaces(sub_stacks);
   var total_sub_width_val = total_sub_width(sub_width, sub_stacks);
-  return (stage_width - total_sub_width_val) / sub_spacing;
+  return ((stage_width - total_sub_width_val) / sub_spacing).toFixed(2);
 };
 
 var calculate_center_spacing = function(stage_width, sub_width, sub_stacks){
   var sub_spacing = sub_spaces(sub_stacks);
   var total_sub_width_val = total_sub_width(sub_width, sub_stacks);
   var rtn_val = ((stage_width - total_sub_width_val) / sub_spacing) + parseInt(sub_width);
-  return isNaN(rtn_val) ? 0 : rtn_val
+  return isNaN(rtn_val) ? 0 : rtn_val.toFixed(2)
 };
 
 document.querySelector(".submit").addEventListener("click", function(e){
